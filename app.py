426b8aa345f53dac456a7a49bdc554127ed53c57
Snixerz <cakeisew@gmail.com>
2019-11-11 12:39:52 -0800

    Added routes

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def say_hello():
-    return "Hello, World!"
+@app.route('/')
+def index():
+    return render_template('fortune.html')
+
+@app.route('/fortune', methods=['GET', 'POST'])
+def fortune():
+    return render_template('index.html')
+
+@app.route('/fortune_results')
+def fortune_results():
+    name = request.args.get('name')
+    drink = request.args.get('drink')
+    animal = request.args.get('animal')
+    city = request.args.get('city')
+    fortune = {
+        'name' :  name,
+        'drink' : drink,
+        'animal' : animal,
+        'city' : city
+    }
+
+    return render_template('fortune_results.html', fortune=fortune)
+if __name__ == '__main__':
+    app.run()
